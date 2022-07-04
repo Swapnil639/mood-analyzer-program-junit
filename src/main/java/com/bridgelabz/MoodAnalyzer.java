b2b205c398ba92bfc7ef8fb393dcf964c363219c
Swapnil639 <swapnilpatil631997@gmail.com>
2022-07-04 06:52:20 +0530

    Added :Check analyze mood happy or sad

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -2,11 +2,14 @@ package com.bridgelabz;
 
 public class MoodAnalyzer {
     public static String analyzeMood(String message){
-        if(message.toLowerCase().contains("sad")){
+        if(message.toLowerCase().contains("sad"))
+        {
              return "SAD";
-        }else if(message.toLowerCase().contains("happy")){
+        }else if(message.toLowerCase().contains("happy"))
+        {
             return "HAPPY";
         }
+
         return null;
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,14 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-    public MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String mood = moodAnalyzer.analyzeMood("I am in sad mood");
         Assert.assertEquals("SAD",mood);
     }
     @Test
     public void givenMessage_WhenSad_ShouldReturnHappy(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
         String mood = moodAnalyzer.analyzeMood("I am in happy mood");
         Assert.assertEquals("HAPPY",mood);
     }
